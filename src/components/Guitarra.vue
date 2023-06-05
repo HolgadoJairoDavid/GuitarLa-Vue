40af963536c7319488ea66a82f323913deab3caa
HolgadoJairoDavid <jairodavidholgado@gamil.com>
2023-06-05 10:32:48 -0300

    learning Vue.js

---
+++ src/components/Guitarra.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref } from 'vue';
+const props = defineProps({
+  guitarra: {
+    type: Object,
+    required: true,
+  },
+});
+
+defineEmits(['agregar-carrito'])
+
+const numero = ref(0)
+</script>
+
+<template>
+  <div class="col-md-6 col-lg-4 my-4 row align-items-center">
+    <div class="col-4">
+      <img
+        class="img-fluid"
+        :src="'/img/' + guitarra.imagen + '.jpg'"
+        :alt="'imagen guitarra ' + guitarra.nombre"
+      />
+    </div>
+    <div class="col-8">
+      <h3 class="text-black fs-4 fw-bold text-uppercase">
+        {{ numero }}
+        {{ guitarra.nombre }}
+      </h3>
+      <p>{{ guitarra.descripcion }}</p>
+      <p class="fw-black text-primary fs-3">${{ guitarra.precio }}</p>
+      <button @click="$emit('agregar-carrito')" type="button" class="btn btn-dark w-100">
+        Agregar al Carrito
+      </button>
+    </div>
+  </div>
+  <!-- FIN GUITARRA -->
+</template>
+
+<style scoped></style>
